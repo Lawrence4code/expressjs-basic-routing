9061f7d4bba43fef6070483a12e94168f79cfbd1
Lawrence <Lawrence4code@gmail.com>
2019-02-05 11:44:47 +0530

    testing1

---
+++ index.js
@@ -0,0 +1,23 @@
+const add = (a, b) => {
+  console.log('add passed');
+};
+
+add(1, 3);
+
+const substract = (a, b) => {
+  console.log('substract passed');
+};
+
+substract();
+
+const multiply = (a, b) => {
+  console.log('multiply pased');
+};
+
+multiply();
+
+const divide = (a, b) => {
+  console.log('divide passed');
+};
+
+divide();
